e6bc9b058d7b50eaf47dad38cc036cd7bfe1b45c
goth1x <142975973+goth1x@users.noreply.github.com>
2023-10-24 15:36:33 +0200

    Update links

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='clean_folder',
-    version='1.0.0',
+    name='clean_folder_ymax',
+    version='0.0.5',
+    description='Organize folder with file types',
     author='Maksym Y',
     author_email='minilond@gmail.com',
+    url='https://github.com/goth1x/goth1x.git',
+    license='MIT',
     packages=find_namespace_packages(),
     entry_points={'console_scripts': [
-        'clean=clean_folder.clean:start']}
+        'clean=clean_folder_ymax.clean:start']}
 )
